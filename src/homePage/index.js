46b1e03ea49223f2d91e3bad96aff28c42cd8268
Christwide Oscar <triple101.games@gmail.com>
2023-04-29 12:45:40 -0500

    Fixed Navigation

---
+++ src/homePage/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Hero from '../components/hero';
+import Grid from '../components/grid';
+
+function Home() {
+    return (
+        <>
+        <Hero/>
+        <Grid/>
+        </>
+    );
+}
+
+export default Home;
