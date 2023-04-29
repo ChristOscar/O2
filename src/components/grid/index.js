af056d55ccbf5b75f684a6b6f3243732573a2fab
Christwide Oscar <triple101.games@gmail.com>
2023-04-29 10:15:22 -0500

    Day 2

--- src/components/grid/index.js
+++ src/components/grid/index.js
@@ -1,19 +1,27 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./Grid.css"; // import the CSS file
-import competition from '../../images/competition.jpg';
-import socialInteraction from '../../images/social-interaction.jpg';
-import equipment from '../../images/equipment.jpg';
-import healthBenefits from '../../images/health-benefits.jpg';
-import nature from '../../images/nature.jpg';
-import physicalActivity from '../../images/physical-activity.jpg';
-import weather from '../../images/weather.jpg';
+import competition from "../../images/competition.jpg";
+import socialInteraction from "../../images/social-interaction.jpg";
+import equipment from "../../images/equipment.jpg";
+import healthBenefits from "../../images/health-benefits.jpg";
+import nature from "../../images/nature.jpg";
+import physicalActivity from "../../images/physical-activity.jpg";
+import weather from "../../images/weather.jpg";
 
 const Grid = () => {
+  const history = useNavigate(); // create history object
+
+  // define click handler function for each grid item
+  const handleClick = (path) => {
+    history(path);
+  };
+
   return (
     <div className="grid-container">
-      <div className="grid-item">
+      <div className="grid-item" onClick={() => handleClick("/nature")}>
         <img src={nature} alt="Nature" />
-        <div className="grid-item-text">Nature1</div>
+        <div className="grid-item-text">Nature</div>
       </div>
       <div className="grid-item">
         <img src={physicalActivity} alt="Physical Activity" />
